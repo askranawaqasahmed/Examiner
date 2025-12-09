d471133c5fec2434e4d2ece1dccad09f744a35b3
ranawaqasahmed <ranawaqasahmed@outlook.com>
2025-12-10 03:44:22 +0500

    swagger expose

--- Source/Ideageek.Examiner.Api/Program.cs
+++ Source/Ideageek.Examiner.Api/Program.cs
@@ -76,11 +76,8 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
